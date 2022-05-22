adcc9d197022b0982d408b1a99bd8aa2f6e735bc
krungy <furler711@gmail.com>
2022-05-22 18:02:39 +0900

    ✨ feat : 작업중인 Header & 이미지파일 추가

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <a href="#">
+      <img src="../static/images/logo_foot.gif" alt="다음 로고" />
+    </a>
+    <form>
+      // 추가 필요
+      <input class="search_main" type="text" />
+    </form>
+  </header>
+</template>
+
+<script>
+export default {
+  components: {},
+};
+</script>
