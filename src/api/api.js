95fe08f31cf1c6ecabdc780dd544eec98e0b50d5
krungy <furler711@gmail.com>
2022-05-29 16:01:47 +0900

    ✨ feat : News api 작업 및 목업 데이터 추가

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,16 @@
 import axios from "axios";
 
-const { API_POINT } = process.env;
+// const { API_POINT } = process.env;
+const url = `https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=8`;
 
 const api = async () => {
-  return await axios.get(API_POINT).then((response) => response);
+  return await axios
+    .get(url, {
+      headers: {
+        "x-api-key": "o-VDgJ98fX1WqNced2OAYqngR9Vaopb0R1DH_r03-Qs",
+      },
+    })
+    .then((response) => response);
 };
 
 export default api;
