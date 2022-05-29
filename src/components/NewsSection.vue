5c57a065d5c1554369ca35fa6578adaebc2f3135
krungy <furler711@gmail.com>
2022-05-29 16:01:12 +0900

    ✨ feat : NewsSection 컴포넌트 추가

---
+++ src/components/NewsSection.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>Hello!</h1>
+</template>
+
+<script>
+import newsApi from "~/api/api";
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      newsList: [],
+    };
+  },
+  mounted() {
+    this.fetchNews();
+  },
+  methods: {
+    async fetchNews() {
+      // const { data } = await newsApi();
+      // console.log(data.articles);
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+$color: red;
+
+h1 {
+  color: $color;
+}
+</style>
